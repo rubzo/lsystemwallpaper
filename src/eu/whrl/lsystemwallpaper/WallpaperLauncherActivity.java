2a128b727b4bba087eef639ae31a5d75466fffe0
Stephen Kyle <jimmykane@gmail.com>
2013-01-07 18:07:05 +0000

    Now works pre-4.1!

--- src/eu/whrl/lsystemwallpaper/WallpaperLauncherActivity.java
+++ src/eu/whrl/lsystemwallpaper/WallpaperLauncherActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import eu.whrl.lsystemwallpaper.R;
@@ -16,9 +17,16 @@ public class WallpaperLauncherActivity extends Activity {
   }
 
   public void onClick(View view) {
-    Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
-    intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
-        new ComponentName(this, LSystemDrawingService.class));
-    startActivity(intent);
+	Intent intent = null;
+	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+		intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+	    intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+	        new ComponentName(this, LSystemDrawingService.class));
+	} else {
+		// Android versions pre-Jelly Bean don't have the above Intent, so we 
+		// can only really take them to the chooser application.
+		intent = new Intent(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
+	}
+	startActivity(intent);
   }
 } 
